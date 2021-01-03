9467c40283636544dda41e3999cb0f1c7ee60926
roke1988 <75531815+roke1988@users.noreply.github.com>
2021-01-03 16:54:58 +0100

    actualizacion script canarias

--- analysis/canarias/canarias_full.R
+++ analysis/canarias/canarias_full.R
@@ -8,47 +8,97 @@ library(jsonlite)
 library(jsonlite)
 library(XML)
 
-#----EXTRAER SERIE DATOS POR HOSPITAL, PROVINCIA E ISLA----
+#1 HOSPITALIZADOS----
+
+url <- "https://grafcan1.maps.arcgis.com/apps/opsdashboard/index.html#/6c18fb18eae64df2a5ecca8c4bd846c3"
+
+#Aquí abrimos un browser (puedes cambiar a "crhome" o "explorer") controlado
+#remotamente desde R
+
+mybrowser <- rsDriver(browser = "firefox")
+
+remDr <- mybrowser[["client"]]
+
+remDr$navigate(url)
+
+#Le decimos a R que espere 9 segundos por si la pagina tarda en cargar
+Sys.sleep(9)
 
 
-# En este caso la dificultad es encontrar el link. Para ello hay que inspeccionar la pagina,
-# ir a Network en la consola, actualizar la pagina y buscar el link con la "query" que necesitamos.
-# Para identificarlo hay que mirar los campos de cada query. En este caso nos interesan 
-# OID, CCV19, FECHA y SERIE (también aparacen en el link)
-# data <- jsonlite::read_json('https://services9.arcgis.com/CgZpnNiCwFObjaOT/arcgis/rest/services/CentrosSanitarios/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=OID%2CCV19%2CFECHA%2CSERIE&orderByFields=FECHA%20asc&resultOffset=0&resultRecordCount=32000&resultType=standard&cacheHint=true') %>%
-#   .$features
+
+dfTEST2 <- tibble()
+
+# Un loop para descargar los datos de cada hospital
+for (i in 1:23) {
+  
+# Le decimos al browser que haga click en un determinado elemento (el nombre del 
+# hospital a la izquierda del panel), para filtrar por isla, y en el siguiente, y 
+# en el siguiente, y en el siguiente...
+
+categorykey_hosp <-remDr$findElement('xpath', paste("/html/body/div/div/div[2]/div/div/div/margin-container/full-container/div[4]/margin-container/full-container/div/div/nav/span[",i,"]", sep = ""))
+
+categorykey_hosp$clickElement()
+
+Sys.sleep(9)
+
+#Descargamos el codigo de la pagina
+
+parsed_pagesource <- remDr$getPageSource()[[1]]
+
+hospi_name <- xml2::read_html(parsed_pagesource) %>%
+  # copiamos el nombre del hospital a traves de su xpath
+  rvest::html_nodes(xpath = paste("/html/body/div/div/div[2]/div/div/div/margin-container/full-container/div[4]/margin-container/full-container/div/div/nav/span[",i,"]", sep = ""))  %>% 
+  html_text() %>% 
+  gsub("\n", "", .) %>% 
+  gsub("[0-9]","",.) %>% 
+  gsub("\\s+", " ",.) %>%  
+  trimws()
 
 
-tmp <- tempfile()
-url <- "https://services9.arcgis.com/CgZpnNiCwFObjaOT/arcgis/rest/services/CentrosSanitarios/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=OID%2CCV19%2CFECHA%2CSERIE&orderByFields=FECHA%20asc&resultOffset=0&resultRecordCount=32000&resultType=standard&cacheHint=true"
-download.file(url, destfile =tmp,quiet = FALSE, mode = "w")
-df <- jsonlite::fromJSON(tmp)
+result_hospi <- xml2::read_html(parsed_pagesource) %>%
+  # aquí se selscciona el tipo de elemento que se quiere extraer, el gráfico a traves de su xpath
+  # Para saber la etiqueta que usar, click derecho sobre uno de los elementos,
+  # dale a inspect, botón derecho y capy/full xpath
+  rvest::html_nodes(xpath = "/html/body/div/div/div[2]/div/div/div/margin-container/full-container/div[15]/margin-container/full-container/div/div/div/div/div[1]/svg/g[12]") 
 
 
+result_hospi2 <- result_hospi %>%
+  html_nodes(css = ".amcharts-graph-bullet")
 
-df <- df[["features"]][["attributes"]]
 
-dfTEST2 <- df %>% 
-  mutate(FECHA = as.character(FECHA)) %>% 
-  mutate(numero = as.numeric(FECHA)) %>% 
-  mutate(date = as.POSIXct(`numero`/1000, origin="1970-01-01")) %>% 
-  group_by(SERIE, date) %>% 
-  # añadimos un ID para cada hospital, aunque aun no sabemos cual es cual
-  mutate(ID = row_number()) %>% 
-  ungroup() %>% 
-  select(-OID, -FECHA, -numero) %>% 
-  spread(SERIE, CV19)
+datos_hospi <- xml_attr(result_hospi2, "aria-label") %>% 
+  as.data.frame() %>% 
+  rename(full_label = 1) %>% 
+  #filter(grepl(paste(c("Casos", "Fallecidos", "Cerrado por alta médica"), collapse="|"), full_label)) %>% 
+  mutate(value = sub(".*\\s", '', full_label)) %>% 
+  mutate(full_label = gsub("\\s+[^ ]+$", '', full_label)) %>% 
+  mutate(date = str_sub(full_label,-12,-1)) %>% 
+  mutate(label_clean = str_sub(full_label,1,-13)) %>% 
+  select(-full_label) %>% 
+  group_by(date, label_clean) %>% 
+  slice(1L) %>% 
+  spread(label_clean, value) %>% 
+  mutate(date = gsub(",","",date)) %>% 
+  mutate(date = as.Date(date, format = "%B %d %Y")) %>% 
+  mutate(hospital = hospi_name)
 
-# Como no hay un ID único para cada hospital, hay que deducir cual es cual en base a los datos
-# previamente recopilados y mirando los campos CCR CSR y HPT. Esto conviene revisarlo periodicamente
-# pues nuestra única guia es el orden en que aparecen los Hospitales y podría cambiar
+dfTEST2 <- rbind(dfTEST2, datos_hospi)
+
+}
+
+# Cierra el browser
+remDr$close()
+rm(mybrowser)
+gc()
+
+
+# Plantilla para vincular los hospitales a las islas
 labels <- tibble(
-  ID = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23),
   Isla = c("Gran Canaria", "Tenerife", "Gran Canaria", "Gran Canaria", "Lanzarote", "Fuerteventura",
            "Gran Canaria", "Gran Canaria", "Lanzarote", "Gran Canaria", "Gran Canaria", "Tenerife",
            "Tenerife", "Tenerife", "Tenerife", "Tenerife", "Tenerife", "La Gomera",
            "El Hierro", "Tenerife", "Gran Canaria", "La Palma", "Tenerife"),
-  Hospital = c("Vithas Hospital Santa Catalina"
+  hospital = c("Vithas Hospital Santa Catalina"
                , "RAMBLA", "Hospital San Roque Las Palmas de Gran Canaria",
                "Hospital Nuestra Señora del Perpetuo Socorro",
                "Hospital Dr. José Molina Orosa",
@@ -62,37 +112,37 @@ labels <- tibble(
                "Hospital San Juan de Dios", "Hospital Quirón Tenerife",
                "Clínica Parque, S.A.", "Hospiten Sur", "Hospiten Bellevue",
                "Hospital Nuestra Señora de Guadalupe",
-               "Hospital Insular Nuestra Señora de los Reyes",
+               "Hospital Insular Nuestra Señora de Los Reyes",
                "Hospital Quirón Costa Adeje",
                "Hospital Policlínico La Paloma, S.A.",
                "Hospital General de La Palma",
                "Complejo Hospitalario Universitario de Canarias")) %>% 
-  select(Isla, Hospital, ID)
+  select(Isla, hospital)
 
 
-
+# Limpiamos y unimos a la plantilla hospitales-islas
 
 output_for_esco_isla <- dfTEST2 %>% 
   left_join(labels) %>% 
-  mutate(HPT = as.character(HPT),
-         CCR = as.character(CCR),
-         CSR = as.character(CSR)) %>% 
-  mutate(HPT = as.numeric(HPT),
-         CCR = as.numeric(CCR),
-         CSR = as.numeric(CSR)) %>% 
+  # mutate(HPT = as.character(HPT),
+  #        CCR = as.character(CCR),
+  #        CSR = as.character(CSR)) %>% 
+  mutate(`Resto de camas hospitalarias ` = as.numeric(`Resto de camas hospitalarias `),
+         `Críticas con respirador ` = as.numeric(`Críticas con respirador `),
+         `Críticas sin respirador ` = as.numeric(`Críticas sin respirador `)) %>% 
   group_by(date, Isla) %>% 
-  summarise(CCR = sum(CCR),
-            HPT = sum(HPT),
-            CSR = sum(CSR)) %>% 
+  summarise(`Críticas con respirador ` = sum(`Críticas con respirador `),
+            `Resto de camas hospitalarias ` = sum(`Resto de camas hospitalarias `),
+            `Críticas sin respirador ` = sum(`Críticas sin respirador `)) %>% 
   ungroup() %>% 
   mutate(date = as.Date(date)) %>% 
   #filter(fecha >= "2020-11-23") %>% 
   #mutate(date = format(as.Date(date, "%d %B %Y"), "%d/%m/%Y")) %>% 
-  rename(Hispitalizados = HPT,
-         `UCI - Críticas con respirador` = CCR,
-         `UCI - Críticas sin respirador` = CSR,
+  mutate(Hospitalizados = `Resto de camas hospitalarias ` + `Críticas con respirador `+`Críticas sin respirador `,
+         `UCI - Críticas con respirador` = `Críticas con respirador `,
+         `UCI - Críticas sin respirador` = `Críticas sin respirador `,
          date = date) %>% 
-  select(date, Isla, Hispitalizados,
+  select(date, Isla, Hospitalizados,
          `UCI - Críticas con respirador`, `UCI - Críticas sin respirador`) %>% 
   arrange(date, match(Isla, c("Tenerife", "Gran Canaria", "Fuerteventura", "Lanzarote", "La Palma", "El Hierro", "La Gomera")))
 
@@ -100,11 +150,11 @@ output_for_esco_isla <- dfTEST2 %>%
 
 #2 CASOS Y FALLECIDOS----
 
-#Para extraer estos datos necesitamos un nuevo dashboard (url). Además, en este
-#caso es un poco más complicado puesto que debemos clickar en diferentes elementos
+#Para extraer estos datos necesitamos un nuevo dashboard (url). 
+#De nuevo debemos clickar en diferentes elementos
 #del dashboard para filtrar por isla
 
-url <- "https://datacovid.salud.aragon.es/covid/"
+url <- "https://grafcan1.maps.arcgis.com/apps/opsdashboard/index.html#/156eddd4d6fa4ff1987468d1fd70efb6"
 
 #Aquí abrimos un browser (puedes cambiar a "crhome" o "explorer") controlado
 #remotamente desde R
@@ -355,7 +405,7 @@ datos_elhierro <- xml_attr(result_elhierro, "aria-label") %>%
   mutate(date = as.Date(date, format = "%B %d %Y")) %>% 
   mutate(Isla = "El Hierro")
 
-# Cierra el growser
+# Cierra el browser
 remDr$close()
 rm(mybrowser)
 gc()
@@ -392,7 +442,7 @@ data_full_final_islas <- casos_islas %>%
   mutate(intensive_care = `UCI - Críticas con respirador` + `UCI - Críticas sin respirador`) %>% 
   select(-`UCI - Críticas sin respirador`, -`UCI - Críticas con respirador`) %>% 
   rename(cases_accumulated = `Casos `, deceased = `Fallecidos `,
-         recovered = `Cerrado por alta médica `, hospitalized = Hispitalizados) %>% 
+         recovered = `Cerrado por alta médica `, hospitalized = Hospitalizados) %>% 
   mutate(cases_accumulated = gsub(",","",cases_accumulated),
          deceased = gsub(",","",deceased),
          recovered = gsub(",","",recovered)) %>% 
@@ -410,7 +460,33 @@ data_full_final_provinces <- data_full_final_islas %>%
             recovered = sum(recovered),
             hospitalized = sum(hospitalized),
             intensive_care = sum(intensive_care)) %>% 
-  ungroup()
+  ungroup() %>% 
+  mutate(province = gsub("Las Palmas", "Palmas, Las", province)) %>% 
+  mutate(ine_code = case_when(
+    province == "Palmas, Las" ~ "35",
+    province == "Santa Cruz de Tenerife" ~ "38",
+    TRUE ~ province
+  ),
+  ccaa = "Canarias", PCR = NA,  TestAc = NA, cases_accumulated_PCR = NA,
+  source_name = "Mapa COVID Canarias ",
+  source = "https://grafcan1.maps.arcgis.com/apps/opsdashboard/index.html#/156eddd4d6fa4ff1987468d1fd70efb6 , 
+  https://grafcan1.maps.arcgis.com/apps/opsdashboard/index.html#/6c18fb18eae64df2a5ecca8c4bd846c3") %>% 
+  group_by(province) %>% 
+  arrange(date) %>% 
+  mutate(cases_accumulated = as.numeric(cases_accumulated)) %>% 
+  mutate(new_cases = cases_accumulated - lag(cases_accumulated),
+         activos =  (cases_accumulated - deceased) - recovered) %>% 
+  ungroup() %>% 
+  mutate(poblacion = case_when(
+    province == "Palmas, Las" ~ "1120406",
+    province == "Santa Cruz de Tenerife" ~ "1032983",
+    TRUE ~ province
+  )) %>% 
+  mutate(poblacion = as.numeric(poblacion)) %>% 
+  select(date, province, ine_code, ccaa, new_cases, PCR, TestAc, hospitalized, intensive_care, 
+         cases_accumulated_PCR, deceased, cases_accumulated, activos, poblacion,
+         recovered, source_name, source)# %>% 
+  #filter(date != max(date))
 
 
 write_csv(data_full_final_islas, "canarias_islas_full.csv")  
