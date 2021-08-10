30de660bee82850377d4ee6c729e70d89c2dd629
puzzle72 <david.rodriguez.mateos@gmail.com>
2021-08-10 22:49:59 +0200

    actualiza datos provincias 2021-08-09

--- analysis/sanidad_pruebas_realizadas/02-desc_datos-hoy.py
+++ analysis/sanidad_pruebas_realizadas/02-desc_datos-hoy.py
@@ -17,12 +17,12 @@ hoy = str(datetime.date.today().strftime('%d%m%Y'))
 
 
 url = url1 + hoy + url3
-url_actual = "../data/Datos_Pruebas_Realizadas_Historico_" + hoy + url3
-url_hist = "../data/Datos_Pruebas_Realizadas_Historico.csv"
+url_actual = "../../data/original/sanidad_pruebas_realizadas/Datos_Pruebas_Realizadas_Historico_" + hoy + url3
+url_hist = "../../data/original/sanidad_pruebas_realizadas/Datos_Pruebas_Realizadas_Historico.csv"
 
 
 try:
-    path = "../data"
+    path = "../../data/original/sanidad_pruebas_realizadas"
     wget.download(url, out=path)
     os.remove(url_hist)
     shutil.copy2(url_actual, url_hist)
